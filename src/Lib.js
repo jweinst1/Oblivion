ffe9e255ea89606a41581cac8749a9ae5f8ffa6c
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-14 14:38:48 -0800

    lib changes

---
+++ src/Lib.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Josh on 2/13/17.
+ * File that holds the standard library
+ */
+"use strict";
+//processes AST nodes
+var Lib;
+(function (Lib) {
+    //retrieves a library function
+    Lib.get = function (AST) {
+        if (Lib.contains(AST))
+            return Lib.defs[AST["node"]];
+    };
+    Lib.contains = function (AST) {
+        return AST["node"] in Lib.defs;
+    };
+    Lib.defs = {};
+})(Lib = exports.Lib || (exports.Lib = {}));
+//# sourceMappingURL=Lib.js.map
---
+++ src/Lib.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by Josh on 2/13/17.
+ * File that holds the standard library
+ */
+
+//processes AST nodes
+export namespace Lib {
+    //retrieves a library function
+    export let get = (AST:Object) => {
+        if(contains(AST)) return defs[AST["node"]];
+    };
+
+    export let contains = (AST:Object) => {
+        return AST["node"] in defs;
+    };
+
+    export let defs = {
+
+    };
+}
