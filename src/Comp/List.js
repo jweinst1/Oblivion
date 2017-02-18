ac54d355ec5812b1d1b549a8bc804933edd16e28
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-17 19:32:42 -0800

    new list methods

--- src/Comp/List.js
+++ src/Comp/List.js
@@ -10,6 +10,36 @@ var Lists;
             this.array = [];
             this.dict = {};
         }
+        //takes one argument
+        List.prototype.append = function (item) {
+            this.array.push(item);
+        };
+        //takes arbitrary amount of arguments
+        List.prototype.appendAll = function () {
+            var items = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                items[_i - 0] = arguments[_i];
+            }
+            Array.prototype.push.apply(this.array, items);
+        };
+        List.prototype.appendLeft = function (item) {
+            this.array.unshift(item);
+        };
+        List.prototype.pop = function () {
+            if (this.array.length > 0)
+                return this.array.pop();
+            else
+                throw "Length Error: No items in list.";
+        };
+        List.prototype.popLeft = function () {
+            if (this.array.length > 0)
+                return this.array.shift();
+            else
+                throw "Length Error: No items in list.";
+        };
+        List.prototype.extend = function (other) {
+            this.array = this.array.concat(other.array);
+        };
         return List;
     }());
     Lists.List = List;
--- src/Comp/List.ts
+++ src/Comp/List.ts
@@ -12,5 +12,34 @@ export namespace Lists {
             this.array = [];
             this.dict = {};
         }
+        //takes one argument
+        append(item:any):void {
+            this.array.push(item)
+        }
+
+        //takes arbitrary amount of arguments
+        appendAll(...items:any[]):void {
+            Array.prototype.push.apply(this.array, items);
+        }
+
+        appendLeft(item:any):void {
+            this.array.unshift(item);
+        }
+
+        pop():any {
+            if(this.array.length > 0) return this.array.pop();
+            //needs custom error
+            else throw "Length Error: No items in list.";
+        }
+
+        popLeft():any {
+            if(this.array.length > 0) return this.array.shift();
+            //needs custom error
+            else throw "Length Error: No items in list.";
+        }
+
+        extend(other:List):void {
+            this.array = this.array.concat(other.array);
+        }
     }
 }
