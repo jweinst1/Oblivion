7a05222fb38a5da2c814bc1ea19d431aa9ce1aec
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-20 11:35:34 -0800

    board update

--- src/svg/Board.js
+++ src/svg/Board.js
@@ -1,4 +1,19 @@
 /**
  * Created by Josh on 2/19/17.
+ * File that implements the SVG Board, which transform into the finished string project
  */
+"use strict";
+var Board;
+(function (Board_1) {
+    var Board = (function () {
+        function Board() {
+            this.css = [];
+            this.svg = [];
+        }
+        return Board;
+    }());
+    Board_1.init = function () {
+        return new Board();
+    };
+})(Board = exports.Board || (exports.Board = {}));
 //# sourceMappingURL=Board.js.map
--- src/svg/Board.ts
+++ src/svg/Board.ts
@@ -1,3 +1,24 @@
 /**
  * Created by Josh on 2/19/17.
+ * File that implements the SVG Board, which transform into the finished string project
  */
+
+
+export namespace Board {
+    class Board {
+
+        public css:any[];
+        public svg:any[];
+
+        constructor(){
+            this.css = [];
+            this.svg = [];
+        }
+    }
+
+    export let init = ():Board => {
+        return new Board();
+    }
+}
+
+
