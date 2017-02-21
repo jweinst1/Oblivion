28baa7f68862821896706d7ec87a55b9cc1883df
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-21 14:23:29 -0800

    pair obj

---
+++ src/Comp/Pair.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Josh on 2/21/17.
+ * File that implements pair object and related features
+ */
+"use strict";
+var Pair;
+(function (Pair_1) {
+    var Pair = (function () {
+        function Pair(first, second) {
+            this.first = first;
+            this.second = second;
+        }
+        return Pair;
+    }());
+    Pair_1.init = function (first, second) {
+        return new Pair(first, second);
+    };
+})(Pair = exports.Pair || (exports.Pair = {}));
+//# sourceMappingURL=Pair.js.map
---
+++ src/Comp/Pair.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by Josh on 2/21/17.
+ * File that implements pair object and related features
+ */
+
+
+export namespace Pair {
+    class Pair {
+        public first:any;
+        public second:any;
+
+        constructor(first:any, second:any){
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    export let init = (first:any, second:any):Pair => {
+        return new Pair(first, second);
+    };
+}
