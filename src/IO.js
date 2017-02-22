2177db0107a0846a025e563aeb307dc129a19814
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-22 15:38:23 -0800

    changed styling/ svg to IO based string writing

--- src/IO.js
+++ src/IO.js
@@ -1,8 +1,9 @@
+"use strict";
+var SVGSize_1 = require("./svg/SVGSize");
 /**
  * Created by Josh on 2/18/17.
  * Small file that handles StdOut
  */
-"use strict";
 //statically encapsulated IO
 var IO;
 (function (IO) {
@@ -10,6 +11,26 @@ var IO;
     var In = "";
     var svg = "";
     var css = "";
+    var xmldat = 'version="1.1" xmlns="http://www.w3.org/2000/svg"';
+    var size = SVGSize_1.SVGSize.init();
+    /*SVG/CSS methods*/
+    IO.pushSVG = function (input) {
+        svg += input + "\n";
+    };
+    IO.pushCSS = function (input) {
+        css += input + "\n";
+    };
+    //returns format string of svg + css
+    IO.getSVGDoc = function (input) {
+        return "<svg " + size.strFormat() + " " + xmldat + ">\n<style>" + css + "</style>\n" + svg + "</svg>";
+    };
+    IO.flushSVG = function () {
+        svg = "";
+    };
+    IO.flushCSS = function () {
+        css = "";
+    };
+    /*StdIn-OUT methods*/
     IO.pushOut = function (input) {
         Out += input + '\n';
     };
--- src/IO.ts
+++ src/IO.ts
@@ -1,3 +1,4 @@
+import {SVGSize} from "./svg/SVGSize";
 /**
  * Created by Josh on 2/18/17.
  * Small file that handles StdOut
@@ -9,7 +10,33 @@ export namespace IO {
     let In = "";
     let svg = "";
     let css = "";
+    let xmldat = 'version="1.1" xmlns="http://www.w3.org/2000/svg"';
+    let size = SVGSize.init();
 
+    /*SVG/CSS methods*/
+
+    export let pushSVG = (input:string):void => {
+        svg += input + "\n";
+    };
+
+    export let pushCSS = (input:string):void => {
+        css += input + "\n";
+    };
+
+    //returns format string of svg + css
+    export let getSVGDoc = (input:string):string => {
+        return `<svg ${size.strFormat()} ${xmldat}>\n<style>${css}</style>\n${svg}</svg>`;
+    };
+
+    export let flushSVG = ():void => {
+        svg = "";
+    };
+
+    export let flushCSS = ():void => {
+        css = "";
+    };
+
+    /*StdIn-OUT methods*/
     export let pushOut = (input:string):void => {
         Out += input + '\n';
     };
