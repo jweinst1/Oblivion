77525cf62c520b13c82aca573776519799ef29bd
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-25 11:40:34 -0800

    started strings

---
+++ src/Comp/Strings.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Josh on 2/25/17.
+ */
+"use strict";
+var Strings;
+(function (Strings) {
+    var OblString = (function () {
+        function OblString(str) {
+            if (str === void 0) { str = ""; }
+            this.str = str;
+        }
+        return OblString;
+    }());
+    Strings.OblString = OblString;
+})(Strings = exports.Strings || (exports.Strings = {}));
+//# sourceMappingURL=Strings.js.map
---
+++ src/Comp/Strings.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Josh on 2/25/17.
+ */
+
+
+export namespace Strings {
+
+    export class OblString {
+        public str:string;
+
+        constructor(str:string = ""){
+            this.str = str;
+        }
+    }
+}
