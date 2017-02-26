68b496dcc5e4dc706011bc783d8dba36abdb102d
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-25 17:42:32 -0800

    more interfaces

--- src/Comp/interfaces.ts
+++ src/Comp/interfaces.ts
@@ -12,10 +12,16 @@ export interface Collection {
 //interface for formatting for printing
 export interface Printable {
     strFormat():string;
+    //gives inner javascript value of Oblivion object
+    innerValue():any;
 }
 
 
 export interface Comparable {
     isEqualTo(other:Comparable):boolean;
     notEqualTo(other:Comparable):boolean;
+}
+
+export interface Array {
+    
 }
