4196286ec6fc0f1f572c1bb1c60f01e491d1ebe1
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-02-27 22:28:41 -0800

    working on maps

--- src/Comp/Maps.js
+++ src/Comp/Maps.js
@@ -1,8 +1,9 @@
+"use strict";
+var Errors_1 = require("../Errors");
 /**
  * Created by Josh on 2/27/17.
  * File that stores the Maps namespace
  */
-"use strict";
 var Maps;
 (function (Maps) {
     var OblMap = (function () {
@@ -10,6 +11,28 @@ var Maps;
             if (dict === void 0) { dict = {}; }
             this.pairs = dict;
         }
+        OblMap.prototype.getItem = function (index) {
+            if (typeof index !== 'object') {
+                if (index in this.pairs)
+                    return this.pairs[index];
+                else
+                    throw new Errors_1.Errors.IndexError(index);
+            }
+            else {
+                if (index.innerValue() in this.pairs)
+                    return this.pairs[index.innerValue()];
+                else
+                    throw new Errors_1.Errors.IndexError(index.innerValue());
+            }
+        };
+        OblMap.prototype.setItem = function (index, value) {
+        };
+        OblMap.prototype.hasItem = function (item) {
+            return undefined;
+        };
+        OblMap.prototype.size = function () {
+            return undefined;
+        };
         return OblMap;
     }());
     Maps.OblMap = OblMap;
--- src/Comp/Maps.ts
+++ src/Comp/Maps.ts
@@ -1,3 +1,5 @@
+import {Collection} from "./interfaces";
+import {Errors} from "../Errors";
 /**
  * Created by Josh on 2/27/17.
  * File that stores the Maps namespace
@@ -5,11 +7,34 @@
 
 
 export namespace Maps {
-    export class OblMap {
+    export class OblMap implements Collection {
         public pairs:Object;
 
         constructor(dict:Object = {}){
             this.pairs = dict
+        }
+
+        getItem(index: any): any {
+            if(typeof index !== 'object'){
+                if(index in this.pairs) return this.pairs[index];
+                else throw new Errors.IndexError(index);
+            }
+            else {
+                if(index.innerValue() in this.pairs) return this.pairs[index.innerValue()];
+                else throw new Errors.IndexError(index.innerValue());
+            }
+        }
+
+        setItem(index: any, value: any): void {
+
+        }
+
+        hasItem(item: any): boolean {
+            return undefined;
+        }
+
+        size(): number {
+            return undefined;
         }
     }
 }
