9dca023aaabd41d39837e747f2b6a516af571fab
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-03-07 18:54:40 -0800

    working on line

--- src/svg/Line.js
+++ src/svg/Line.js
@@ -2,6 +2,21 @@
 var Line;
 (function (Line) {
     Line.makeLine = function (env, args) {
+        switch (args.length) {
+            case 0:
+                throw new Error("ArgumentError: Expected at least 2 arguments but got " + args.length);
+            case 1:
+                var arg = env.callLib(env, args[0].node, args[0].args);
+                if (typeof arg !== 'object')
+                    throw new Error("TypeError: Expected list argument, got " + typeof arg);
+        }
+    };
+    //private method
+    Line.polyLineString = function (points) {
+        if (points.length % 2 !== 0)
+            throw new Error("ArgCountError: A Line must have an even number of arguments");
+        var str = "";
+        return str;
     };
 })(Line = exports.Line || (exports.Line = {}));
 //# sourceMappingURL=Line.js.map
--- src/svg/Line.ts
+++ src/svg/Line.ts
@@ -7,6 +7,20 @@ import {Environment} from "../Env";
 export namespace Line {
 
     export let  makeLine = (env:Environment.Env, args:any[]) => {
+        switch(args.length){
+            case 0:
+                throw new Error(`ArgumentError: Expected at least 2 arguments but got ${args.length}`);
+            case 1:
+                let arg = env.callLib(env, args[0].node, args[0].args);
+                if(typeof arg !== 'object') throw new Error(`TypeError: Expected list argument, got ${typeof arg}`);
 
+        }
+    };
+
+    //private method
+    export let polyLineString = (points:any[]):string => {
+        if(points.length % 2 !== 0) throw new Error("ArgCountError: A Line must have an even number of arguments");
+        let str = "";
+        return str;
     };
 }
