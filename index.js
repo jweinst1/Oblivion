017d16598c108622631c588c04a2042baac62514
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-03-09 10:12:23 -0800

    index file updated

--- index.js
+++ index.js
@@ -4,3 +4,17 @@
 var cmp = require('./src/Main');
 var fs = require('fs');
 
+var userArgs = process.argv.slice(2);
+
+//reads string from file
+fs.readFile(userArgs[0], 'utf-8', function (err, data) {
+    if (err) throw err;
+    if(userArgs.length === 2) {
+        fs.writeFile(userArgs[1], cmp.Compile(data, 0), function (err) {
+            if (err) throw err;
+        });
+    }
+    else fs.writeFile(userArgs[1], cmp.Compile(data), function (err) {
+        if (err) throw err;
+    });
+});
