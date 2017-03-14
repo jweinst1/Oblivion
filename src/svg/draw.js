f01eb93f0d1b4ac2a8e03bc6fb78162d52547c5f
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-03-13 17:46:55 -0700

    finished line connect

--- src/svg/draw.js
+++ src/svg/draw.js
@@ -1,4 +1,5 @@
 "use strict";
+var Line_1 = require("./Line");
 /**
  * Created by Josh on 2/23/17.
  * Contains implementation for the draw: keyword and underlying function
@@ -10,6 +11,29 @@ var Draw;
     Draw.lineConnect = function (env, args) {
         var left = env.callLib(env, args[0].node, args[0].args);
         var right = env.callLib(env, args[1].node, args[1].args);
+        if (left.type() === 'point') {
+            if (right.type() === 'point') {
+                return new Line_1.Lines.Line(left, new Line_1.Lines.Line(right));
+            }
+            else if (right.type() === 'line') {
+                return new Line_1.Lines.Line(left, right);
+            }
+            else
+                throw new Error("-> Operator received wrong arguments");
+        }
+        else if (left.type() === 'line') {
+            if (right.type() === 'point') {
+                left.getLast().next = new Line_1.Lines.Line(right);
+                return left;
+            }
+            else if (right.type() === 'line') {
+                left.getLast().next = right;
+            }
+            else
+                throw new Error("-> Operator received wrong arguments");
+        }
+        else
+            throw new Error("-> Operator received wrong arguments");
     };
 })(Draw = exports.Draw || (exports.Draw = {}));
 //# sourceMappingURL=draw.js.map
--- src/svg/draw.ts
+++ src/svg/draw.ts
@@ -1,5 +1,6 @@
 import {Environment} from "../Env";
 import {IO} from "../IO";
+import {Lines} from "./Line";
 /**
  * Created by Josh on 2/23/17.
  * Contains implementation for the draw: keyword and underlying function
@@ -11,5 +12,25 @@ export namespace Draw {
     export let lineConnect = (env:Environment.Env, args:any[]) => {
         let left = env.callLib(env, args[0].node, args[0].args);
         let right = env.callLib(env, args[1].node, args[1].args);
+        if(left.type() === 'point'){
+            if(right.type() === 'point') {
+                return new Lines.Line(left, new Lines.Line(right));
+            }
+            else if(right.type() === 'line'){
+                return new Lines.Line(left, right);
+            }
+            else throw new Error(`-> Operator received wrong arguments`);
+        }
+        else if(left.type() === 'line'){
+            if(right.type() === 'point'){
+                left.getLast().next = new Lines.Line(right);
+                return left;
+            }
+            else if(right.type() === 'line'){
+                left.getLast().next = right;
+            }
+            else throw new Error(`-> Operator received wrong arguments`);
+        }
+        else throw new Error(`-> Operator received wrong arguments`);
     }
 }
