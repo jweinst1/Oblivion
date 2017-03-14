7641c2626e05bb921d5519155a485b746cd91cf5
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-03-14 00:23:31 -0700

    working on wrappers

--- src/svg/Wrappers.js
+++ src/svg/Wrappers.js
@@ -1,6 +1,23 @@
+"use strict";
 /**
  * Created by Josh on 3/13/17.
  * File to contain objects that assist in SVG string synthesis.
  */
-"use strict";
+var Wrappers;
+(function (Wrappers) {
+    var Line = (function () {
+        function Line() {
+            this.pointStr = "";
+            this.stroke = "black";
+            this.strokeWidth = 1;
+        }
+        Line.prototype.put = function (item) {
+        };
+        Line.prototype.getSVGString = function () {
+            return undefined;
+        };
+        return Line;
+    }());
+    Wrappers.Line = Line;
+})(Wrappers = exports.Wrappers || (exports.Wrappers = {}));
 //# sourceMappingURL=Wrappers.js.map
--- src/svg/Wrappers.ts
+++ src/svg/Wrappers.ts
@@ -1,8 +1,28 @@
+import {SVGWrapper} from "./Interfaces";
+import {SVGPolyObject} from "./Interfaces";
 /**
  * Created by Josh on 3/13/17.
  * File to contain objects that assist in SVG string synthesis.
  */
 
 export namespace Wrappers {
+    export class Line implements SVGWrapper {
+        public pointStr:string;
+        public stroke:string;
+        public strokeWidth:number;
 
+        constructor(){
+            this.pointStr = "";
+            this.stroke = "black";
+            this.strokeWidth = 1;
+        }
+
+        put(item: SVGPolyObject): void {
+
+        }
+
+        getSVGString(): string {
+            return undefined;
+        }
+    }
 }
