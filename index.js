2e2d89ebcee01a6bfa34aa8b80a7db3d4f270e9e
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-03-17 14:51:59 -0700

    main function change

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ var cmp = require('./src/Main');
 var fs = require('fs');
 
 var userArgs = process.argv.slice(2);
+if(userArgs.length !== 1) throw new Error("Invalid Command Line Arguments");
 
 //reads string from file
 fs.readFile(userArgs[0], 'utf-8', function (err, data) {
     if (err) throw err;
-    if(userArgs.length === 2) {
-        fs.writeFile(userArgs[1], cmp.Compile(data, 0), function (err) {
-            if (err) throw err;
-        });
-    }
-    else fs.writeFile(convertFileName(userArgs[0]), cmp.Compile(data), function (err) {
+    var output = cmp.Compile(data, 2);
+    console.log(output[0]);
+     fs.writeFile(convertFileName(userArgs[0]), output[1], function (err) {
         if (err) throw err;
     });
 });
 
 
 function convertFileName(string){
-    return string.match(/(.+)\.obl/)[1] + ".svg"
+    if(!/(.+)\.obl/.test(string)) throw new Error("Only Oblivion files with a .obl extension can be compiled");
+    return string.match(/(.+)\.obl/)[1] + ".svg";
 }
